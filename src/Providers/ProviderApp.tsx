30e823e68288f17c1c8f9d951bf69adaba34bd3c
onelaht <covercoverunder@gmail.com>
2026-02-01 11:43:03 -0500

    Refactor provider for filters and map markers

--- src/Providers/ProviderApp.tsx
+++ src/Providers/ProviderApp.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ProviderStations from "./ProviderStations.tsx";
+import ProviderInitializer from "./ProviderInitializer.tsx";
 
 export default function ProviderApp({children}:{children: React.ReactNode}) {
     return (
-        <ProviderStations>
-            {children}
-        </ProviderStations>
+        <ProviderInitializer>
+            <ProviderStations>
+                {children}
+            </ProviderStations>
+        </ProviderInitializer>
     )
 }
