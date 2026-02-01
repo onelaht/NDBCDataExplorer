4c4e3dc0866cab8add4119d1cc4737e8dde22251
onelaht <covercoverunder@gmail.com>
2026-02-01 11:46:08 -0500

    Render loading screen

--- src/FLComponents/MapLayer.tsx
+++ src/FLComponents/MapLayer.tsx
@@ -1,5 +1,5 @@
 // react
-import {useCallback, useEffect} from "react";
+import {useState} from "react";
 // react leaflet components
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 // leaflet types
@@ -7,76 +7,22 @@ import type {LatLngExpression} from "leaflet";
 import L from "leaflet";
 // leaflet styling
 import 'leaflet/dist/leaflet.css'
-import {useStations} from "../Providers/ProviderStations.tsx";
 // react router
 import {useNavigate} from "react-router-dom";
-// types and interfaces
-import type {Initializer} from "../../types/Initializer.ts";
-import type {IStation} from "../../types/IStation.ts";
-import type {IStationData} from "../../types/IStationData.ts";
 // google svg icon
 import SupportIcon from "../SVGIcons/MUI_Support_B89230.svg"
+// global vars
+import {useInitializer} from "../Providers/ProviderInitializer.tsx";
+// loader component
+import MapLayerLoader from "./Loader/MapLayerLoader.tsx";
 
 export default function MapLayer() {
     // route to specific station id
     const nav = useNavigate();
     // global vars
-    const {setStations, setUniqueOwners, setSelCountries,
-        setSelOwners, setUniqueCountries, filteredStations} = useStations();
-
-    // initializes country and owner filter
-    // - if at least 1 unique data exists, initializes map for either filter type
-    // - if no unique data exists, ignores initialization
-    const initSelectedValues = useCallback((c: string[],
-                                            o:Pick<IStationData, "code" | "owner_name">[]) => {
-        // if unique country data exists
-        if(c.length > 0) {
-            // assign as map
-            setSelCountries(prev => {
-                const temp = new Map<string, boolean>(prev);
-                c.forEach(i => {
-                    if(Object.values(i)[0] === "US") temp.set(Object.values(i)[0], true);
-                    else temp.set(Object.values(i)[0], false);
-                })
-                return temp;
-            })
-        }
-        // if unique owners data exists
-        if(o.length > 0) {
-            // assign as map
-            setSelOwners(prev => {
-                const temp = new Map<string, boolean>(prev);
-                o.forEach(i => {
-                    temp.set(Object.values(i)[0], false);
-                })
-                return temp;
-            })
-        }
-    }, [setSelCountries, setSelOwners])
-
-    // initialize stations arr by fetching station_table db
-    const fetchStations = useCallback(async() => {
-        // fetch via endpoint
-        const res = await fetch(`/api/stations/`)
-        // if unsuccessful throw error
-        if(!res.ok) {
-            throw new Error("Failed to fetch stations.");
-        }
-        // get data
-        const result:Initializer = await res.json();
-        // assign data
-        if(result?.stations?.length > 0) {
-            const tempMap = new Map<string, IStationData>();
-            result.stations.forEach((i:IStation) => {
-                tempMap.set(i.station_id, i.data);
-            })
-            setStations(tempMap);
-        }
-        setUniqueCountries(result?.uCountries ?? []);
-        setUniqueOwners(result?.uOwners ?? []);
-        // initialize selected values
-        initSelectedValues(result?.uCountries ?? [], result?.uOwners ?? []);
-    }, [initSelectedValues, setStations, setUniqueCountries, setUniqueOwners])
+    const {filteredMarkers} = useInitializer();
+    // fetcher
+    const [isFetched, setIsFetched] = useState<boolean>(false);
 
     // mui icon for leaflet markers
     const muiMarkerIcon = L.icon({
@@ -84,30 +30,32 @@ export default function MapLayer() {
         iconSize:[32, 32],
     })
 
-    // run fetcher at start
-    useEffect(() => {
-        fetchStations();
-    }, [])
-
     return (
-        <MapContainer worldCopyJump={true} center={[51, -0]} zoom={13} style={{width: "100%", height: "100%"}}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {filteredStations.size > 0 && [...filteredStations].map(([k,v]) => (
-                <Marker
-                    icon={muiMarkerIcon}
-                    position={v.location as LatLngExpression}
-                    eventHandlers={{
-                        click: () => {
-                            nav(`/${k}`)
-                        }
-                    }}
-                >
-                    <Popup>Station Name: {k}</Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+        <>
+            { !isFetched ?
+                <MapLayerLoader setIsFetched={val => setIsFetched(val)}/>
+                :
+                <MapContainer worldCopyJump={true} center={[51, -0]} zoom={13} style={{width: "100%", height: "100%"}}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {filteredMarkers.map((i) => (
+                        <Marker
+                            icon={muiMarkerIcon}
+                            position={i.location as LatLngExpression}
+                            eventHandlers={{
+                                click: () => {
+                                    nav(`/${i.station_id}`)
+                                }
+                            }}
+                        >
+                            <Popup>Station ID: {i.station_id}</Popup>
+                        </Marker>
+                    ))}
+                </MapContainer>
+
+            }
+        </>
     )
 }
