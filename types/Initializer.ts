f72d980ef7f9e82e7d3ff6f30f5f82eb79b3682f
onelaht <covercoverunder@gmail.com>
2026-02-02 11:00:13 -0500

    Rename var

--- types/Initializer.ts
+++
@@ -1,8 +0,0 @@
-import type {IStation} from "./IStation.ts";
-import type {IStationData} from "./IStationData.ts";
-
-export type Initializer = {
-    stations: IStation[];
-    uCountries: Array<IStationData["country_code"]>;
-    uOwners: Array<Pick<IStationData, "code" | "owner_name">>
-}
