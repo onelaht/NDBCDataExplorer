8f02376167808ce9a3ac6de569e5c8d130a84b4d
onelaht <covercoverunder@gmail.com>
2026-02-05 10:37:39 -0500

    Fix incorrect table name

--- worker/db/stations_datatype.ts
+++ worker/db/stations_datatype.ts
@@ -10,8 +10,8 @@ export function stations_datatype(db:D1Database) {
         // prepare statement for upsert
         const req = db.prepare(
             "INSERT INTO " +
-            "stations (station_id, adcp, cwind, dart, drift, ocean, rain, spec, spectral, srad, supl, txt, last_updated)" +
-            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp)" +
+            "stations_datatype (station_id, adcp, cwind, dart, drift, ocean, rain, spec, spectral, srad, supl, txt, last_updated)" +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp) " +
             "ON CONFLICT(station_id) DO UPDATE SET " +
             "station_id = excluded.station_id, " +
             "adcp = excluded.adcp, " +
@@ -27,11 +27,11 @@ export function stations_datatype(db:D1Database) {
             "txt = excluded.txt, " +
             "last_updated = excluded.last_updated " +
             "WHERE " +
-            "stations_datatype.adcp IS DISTINCT FROM excluded.acdp OR " +
+            "stations_datatype.adcp IS DISTINCT FROM excluded.adcp OR " +
             "stations_datatype.cwind IS DISTINCT FROM excluded.cwind OR " +
             "stations_datatype.dart IS DISTINCT FROM excluded.dart OR " +
             "stations_datatype.drift IS DISTINCT FROM excluded.drift OR " +
-            "stations_datatype.ocean IS DISTINCT FROM excluded.ocean " +
+            "stations_datatype.ocean IS DISTINCT FROM excluded.ocean OR " +
             "stations_datatype.rain IS DISTINCT FROM excluded.rain OR " +
             "stations_datatype.spec IS DISTINCT FROM excluded.spec OR " +
             "stations_datatype.spectral IS DISTINCT FROM excluded.spectral OR " +
