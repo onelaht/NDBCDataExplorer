f6ed3d3828b5df3b56faeeaeea7b6a7a43436540
onelaht <covercoverunder@gmail.com>
2026-02-20 10:12:04 -0500

    Implement loaders per datatype

--- src/FLComponents/Loader/ChartLoader/MeteorologicalDriftChartLoader.tsx
+++ src/FLComponents/Loader/ChartLoader/MeteorologicalDriftChartLoader.tsx
@@ -1,14 +1,14 @@
 // react
 import React, {useCallback, useEffect} from "react";
 // types and interfaces
-import type {IMeteorologicalDriftData} from "../../../types/IMeteorologicalDriftData.ts";
+import type {IMeteorologicalDriftData} from "../../../../types/IMeteorologicalDriftData.ts";
 // react router (get router path)
 import {useLocation} from "react-router-dom";
 // child component
-import BaseLoader from "./BaseLoader.tsx";
+import BaseLoader from "../BaseLoader.tsx";
 
-export default function MeteorologicalDriftChartLoader({setData, setIsFetched}:
-    {setData: React.Dispatch<React.SetStateAction<IMeteorologicalDriftData[]>>,
+export default function MeteorologicalDriftChartLoader({setTypeData, setIsFetched}:
+    {setTypeData: React.Dispatch<React.SetStateAction<IMeteorologicalDriftData[]>>,
      setIsFetched:React.Dispatch<React.SetStateAction<boolean>>}) {
     // get router path
     const {pathname} = useLocation();
@@ -28,9 +28,10 @@ export default function MeteorologicalDriftChartLoader({setData, setIsFetched}:
             throw new Error(res.statusText);
         else {
             const data: IMeteorologicalDriftData[] = await res.json();
-            setData(data);
+            console.log(data);
+            setTypeData(data);
         }
-    }, [pathname, setData])
+    }, [pathname, setTypeData])
 
     useEffect(() => {
         fetchData().then(() => setIsFetched(true));
